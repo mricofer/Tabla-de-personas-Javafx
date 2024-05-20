dfbe11d7250351131d0ae19a49ad857b52abc20d
Miguel Rico <mricofernandez@safareyes.es>
2024-05-20 13:19:27 +0200

    ✨ Add delete and modify functionality

--- src/main/java/controler/PersonaControler.java
+++ src/main/java/controler/PersonaControler.java
@@ -8,8 +8,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import model.Persona;
 
+import java.io.BufferedReader;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -31,6 +33,10 @@ public class PersonaControler implements Initializable {
     private TableColumn colApellidos;
     @FXML
     private TableColumn colEdad;
+    @FXML
+    private Button btnEliminar;
+    @FXML
+    private Button btnModificar;
 
     private ObservableList<Persona> personas;
 
@@ -71,6 +77,75 @@ public class PersonaControler implements Initializable {
             alert.setTitle("Error");
             alert.setContentText("La edad debe ser un número entero");
             alert.showAndWait();
+        }
+
+    }
+
+    @FXML
+    private void seleccionar(MouseEvent event){
+        Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
+
+        if(p != null){
+            this.txtNombre.setText(p.getNombre());
+            this.txtApellidos.setText(p.getApellidos());
+            this.txtEdad.setText(String.valueOf(p.getEdad()));
+        }
+    }
+    @FXML
+    private void eliminarPersona(ActionEvent event){
+        Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
+
+        if(p == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("No se ha seleccionado ninguna persona");
+            alert.showAndWait();
+        }else{
+            this.personas.remove(p);
+            this.tblPersonas.refresh();
+        }
+    }
+    @FXML
+    private void modificarPersona(ActionEvent event){
+
+        Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
+
+        if(p == null){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("No se ha seleccionado ninguna persona");
+            alert.showAndWait();
+        }else{
+            try{
+                String nombre = this.txtNombre.getText();
+                String apellidos = this.txtApellidos.getText();
+                int edad = Integer.parseInt(this.txtEdad.getText());
+
+                Persona aux = new Persona(nombre, apellidos, edad);
+
+                if(!this.personas.contains(aux)){
+                    p.setNombre(aux.getNombre());
+                    p.setApellidos(aux.getApellidos());
+                    p.setEdad(aux.getEdad());
+
+                    this.tblPersonas.refresh();
+
+                }else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setHeaderText(null);
+                    alert.setTitle("Error");
+                    alert.setContentText("La persona ya existe");
+                    alert.showAndWait();
+                }
+            } catch (NumberFormatException e){
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                alert.setContentText("La edad debe ser un número entero");
+                alert.showAndWait();
+            }
         }
 
     }
