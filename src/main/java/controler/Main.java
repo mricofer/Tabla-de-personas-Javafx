251229da6492f2ea8083211d9c0103c91baa1929
Miguel Rico <mricofernandez@safareyes.es>
2024-05-20 12:35:11 +0200

    ♻️ Modify the paths to some files and start to make the table and the functions to work correctly

--- src/main/java/controler/Main.java
+++ src/main/java/controler/Main.java
@@ -1,4 +1,27 @@
 package controler;
 
-public class Main {
+import java.io.IOException;
+import static javafx.application.Application.launch;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/tablaPersonas.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
-module com.example.demo1 {
+module com.example.tablaPersonas {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
 
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
+    opens controler to javafx.fxml;
+    exports controler;
 }
