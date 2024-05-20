3dadcf61a39057709fef2b29dd6722c9cf50c54b
Miguel Rico <mricofernandez@safareyes.es>
2024-05-20 12:36:50 +0200

    ♻️ Modify the paths to some files and start to make the table and the functions to work correctly

---
+++ src/main/java/controler/PersonaControler.java
@@ -0,0 +1,74 @@
+package controler;
+
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Persona;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PersonaControler implements Initializable {
+
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellido;
+    @FXML
+    private TextField txtEdad;
+    @FXML
+    private Button btnAgregar;
+    @FXML
+    private TableView<Persona> tblPersonas;
+    @FXML
+    private TableColumn colNombre;
+    @FXML
+    private TableColumn colApellidos;
+    @FXML
+    private TableColumn colEdad;
+
+    private ObservableList<Persona> personas;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        personas = FXCollections.observableArrayList();
+
+        this.colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        this.colApellidos.setCellValueFactory(new PropertyValueFactory("apellidos"));
+        this.colEdad.setCellValueFactory(new PropertyValueFactory("edad"));
+    }
+
+    private void agregarPersona(ActionEvent event){
+
+        try{
+            String nombre = this.txtNombre.getText();
+            String apellidos = this.txtApellido.getText();
+            int edad = Integer.parseInt(this.txtEdad.getText());
+
+            Persona p = new Persona(nombre, apellidos, edad);
+
+            if(!this.personas.contains(p)){
+                this.personas.add(p);
+                this.tblPersonas.setItems(personas);
+            }else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setTitle("Error");
+                alert.setContentText("La persona ya existe");
+                alert.showAndWait();
+            }
+        } catch (NumberFormatException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setTitle("Error");
+            alert.setContentText("La edad debe ser un número entero");
+            alert.showAndWait();
+        }
+
+    }
+}
